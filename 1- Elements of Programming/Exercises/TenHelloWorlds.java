3fe8b3a20b10407a52bc7fa487ce76c5592780f3
Kenan Polat <kenanpol.ce@gmail.com>
2016-01-03 16:22:15 +0200

    Section 1

---
+++ 1- Elements of Programming/Exercises/TenHelloWorlds.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Kenan Polat on 02.01.2016
+ */
+
+/******************************************************************************
+ *  Compilation:  javac TenHelloWorlds.java
+ *  Execution:    java TenHelloWorlds
+ *
+ *  Prints "Hello, World" ten times. You'll learn about a more
+ *  compact way to do this in Section 1.3 when we introduce loops.
+ *
+ *  % java TenHelloWorlds
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *  Hello, World
+ *
+ ******************************************************************************/
+
+public class TenHelloWorlds {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+
+}
